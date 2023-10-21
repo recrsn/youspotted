8e347f27a2d8a693e639845b1b224f79a2657f9a
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2023-10-22 04:10:39 +0530

    Fix github logo

--- build/register-sw.js
+++
@@ -1,9 +0,0 @@
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js')
-        .then(function (registration) {
-            console.log('Service worker registration succeeded:', registration);
-        })
-        .catch(function (error) {
-            console.log('Service worker registration failed:', error);
-        });
-}
